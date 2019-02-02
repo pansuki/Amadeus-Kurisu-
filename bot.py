036ef3eee0ab3038347612fdaba3f0d721687631
pansuki <47238242+pansuki@users.noreply.github.com>
2019-02-02 11:51:45 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,11 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
-
+import systems as sys
 #import random
+
+bot_token = Enter your bot token here' #to host code on Github, change to bot_token=os.environ['BOT_TOKEN'] then add a config var called BOT_TOKEN and put your bot token as the value
+loaded={'systems':False,'msg_track':False}
 
 bot = commands.Bot(command_prefix=';')
 
